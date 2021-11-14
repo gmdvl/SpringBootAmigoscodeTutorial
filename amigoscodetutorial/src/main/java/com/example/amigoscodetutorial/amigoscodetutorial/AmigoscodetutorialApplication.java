71c507cb2f61f4c20496a8eb828db393342cfd37
gmdvl <gabrielmdvl@gmail.com>
2021-11-14 17:24:39 -0400

    first commit

---
+++ amigoscodetutorial/src/main/java/com/example/amigoscodetutorial/amigoscodetutorial/AmigoscodetutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodetutorial.amigoscodetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodetutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodetutorialApplication.class, args);
+	}
+
+}
---
+++ amigoscodetutorial/src/test/java/com/example/amigoscodetutorial/amigoscodetutorial/AmigoscodetutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodetutorial.amigoscodetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodetutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
